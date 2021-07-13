84ca74e1f89b2b483ce03037bf6baacf67f71386
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2021-07-13 12:50:00 +0200

    Add a command line interface to select the script.

--- finalizer.lua
+++ finalizer.lua
@@ -5,14 +5,27 @@ local pl = t.pl
 t:install{
   ['local/netX90_reset_NXJTAG-4000-USB.tcl']    = '${install_base}/',
   ['local/netX500_reset_NXHX_onboard_FTDI.tcl'] = '${install_base}/',
-  ['local/reset.lua']                           = '${install_base}/',
 
   ['${report_path}']                            = '${install_base}/.jonchki/'
 }
+
+-- Read the "reset.lua" script and replace all "${}" expressions.
+-- This is used to insert the version number.
+local strSrcFile = pl.path.abspath('local/reset.lua', t.strCwd)
+local strTemplate, strMessage = pl.utils.readfile(strSrcFile)
+if strTemplate==nil then
+  error('Failed to read ' .. strSrcFile .. ' : '..tostring(strMessage))
+end
+local strText = t:replace_template(strTemplate)
+local strDstFile = t:replace_template('${install_base}/reset.lua')
+local fResult
+fResult, strMessage = pl.utils.writefile(strDstFile, strText)
+if fResult~=true then
+  error('Failed to write to ' .. tostring(strDstFile) .. ' : ' .. tostring(strMessage))
+end
 
 t:createPackageFile()
 t:createHashFile()
 t:createArchive('${install_base}/../../../${default_archive_name}', 'native')
 
 return true
-
--- local/reset.lua
+++ local/reset.lua
@@ -1,45 +1,157 @@
-require 'muhkuh_cli_init'
+local __APPLICATION__ = 'netx_reset'
+local __VERSION__ = '${root_artifact_version}'
 
+local argparse = require 'argparse'
 local openocd = require 'luaopenocd'
+local pl = require'pl.import_into'()
 
--- Read the TCL script.
-local tFile = io.open('netX90_reset_NXJTAG-4000-USB.tcl', 'r')
-local strScript = tFile:read('*a')
-tFile:close()
+local atLogLevels = {
+  'debug',
+  'info',
+  'warning',
+  'error',
+  'fatal'
+}
+local atTranslateLogLevels = {
+  ['debug'] = 'debug',
+  ['info'] = 'info',
+  ['warning'] = 'warning',
+  ['error'] = 'error',
+  ['fatal'] = 'fatal'
+}
 
-local function msg_callback(strMsg)
-  print(string.format('[openOCD] %s', tostring(strMsg)))
-end
-local tOpenOCD = openocd.luaopenocd(msg_callback)
-tOpenOCD:initialize()
+local tParser = argparse(__APPLICATION__, 'Reset a netX board.')
+tParser:flag('--version')
+  :description('Show the version and exit.')
+  :action(function()
+    print(string.format('%s %s', __APPLICATION__, __VERSION__))
+    print('Copyright (C) 2021 by Christoph Thelen (doc_bacardi@users.sourceforge.net)')
+    os.exit(0)
+  end)
+tParser:argument('script')
+  :argname('<TCL_SCRIPT_FILE>')
+  :description('Read the TCL script from TCL_SCRIPT_FILE.')
+  :target('strTclScriptFile')
+tParser:option('-v --verbose')
+  :description(string.format('Set the verbosity level to LEVEL. Possible values for LEVEL are %s.', table.concat(atLogLevels, ', ')))
+  :argname('<LEVEL>')
+  :convert(atTranslateLogLevels)
+  :default('warning')
+  :target('strLogLevel')
+tParser:option('-l --logfile')
+  :description('Write all output to FILE.')
+  :argname('<FILE>')
+  :default(nil)
+  :target('strLogFileName')
+tParser:mutex(
+  tParser:flag('--color')
+    :description('Use colors to beautify the console output. This is the default on Linux.')
+    :action("store_true")
+    :target('fUseColor'),
+  tParser:flag('--no-color')
+    :description('Do not use colors for the console output. This is the default on Windows.')
+    :action("store_false")
+    :target('fUseColor')
+)
+local tArgs = tParser:parse()
 
-local strResult
-local iResult = tOpenOCD:run(strScript)
-if iResult~=0 then
-  error('Failed to execute the script.')
-else
-  strResult = tOpenOCD:get_result()
-  print(string.format('Script result: %s', strResult))
-  if strResult=='0' then
-    fOK = true
+-----------------------------------------------------------------------------
+--
+-- Create a log writer.
+--
+
+local fUseColor = tArgs.fUseColor
+if fUseColor==nil then
+  if pl.path.is_windows==true then
+    -- Running on windows. Do not use colors by default as cmd.exe
+    -- does not support ANSI on all windows versions.
+    fUseColor = false
   else
-    tLog.debug('The script result is not "0".')
+    -- Running on Linux. Use colors by default.
+    fUseColor = true
   end
 end
 
-print('Uninitialize OpenOCD.')
+-- Collect all log writers.
+local atLogWriters = {}
+
+-- Create the console logger.
+local tLogWriterConsole
+if fUseColor==true then
+  tLogWriterConsole = require 'log.writer.console.color'.new()
+else
+  tLogWriterConsole = require 'log.writer.console'.new()
+end
+table.insert(atLogWriters, tLogWriterConsole)
+
+-- Create the file logger if requested.
+local tLogWriterFile
+if tArgs.strLogFileName~=nil then
+  tLogWriterFile = require 'log.writer.file'.new{ log_name=tArgs.strLogFileName }
+  table.insert(atLogWriters, tLogWriterFile)
+end
+
+-- Combine all writers.
+local tLogWriter
+if _G.LUA_VER_NUM==501 then
+  tLogWriter = require 'log.writer.list'.new(unpack(atLogWriters))
+else
+  tLogWriter = require 'log.writer.list'.new(table.unpack(atLogWriters))
+end
+
+-- Set the logger level from the command line options.
+local cLogWriter = require 'log.writer.filter'.new(tArgs.strLogLevel, tLogWriter)
+local tLog = require "log".new(
+  -- maximum log level
+  "trace",
+  cLogWriter,
+  -- Formatter
+  require "log.formatter.format".new()
+)
+
+-- Read the TCL script.
+local strTclScript, strError = pl.utils.readfile(tArgs.strTclScriptFile, false)
+if strTclScript==nil then
+  tLog.error('Failed to read the TCL script "%s": %s.', tArgs.strTclScriptFile, tostring(strError))
+  error('Failed to read the TCL script.')
+end
+
+local function msg_callback(strMsg)
+  tLog.debug('[openOCD] %s', tostring(strMsg))
+end
+local tOpenOCD = openocd.luaopenocd(msg_callback)
+tLog.debug('Initializing OpenOCD.')
+tOpenOCD:initialize()
+
+local strResult
+local fOK
+local iResult = tOpenOCD:run(strTclScript)
+if iResult~=0 then
+  tLog.error('Failed to execute the script.')
+else
+  strResult = tOpenOCD:get_result()
+  tLog.debug('Script result: %s', strResult)
+  if strResult=='0' then
+    fOK = true
+  else
+    tLog.error('The script failed with the result %s.', tostring(strResult))
+  end
+end
+
+
+tLog.debug('Uninitialize OpenOCD.')
 tOpenOCD:uninit()
 
 if fOK~=true then
-  error('The script did not succeed.')
+  tLog.error('The script did not succeed.')
 else
-  print('')
-  print(' #######  ##    ## ')
-  print('##     ## ##   ##  ')
-  print('##     ## ##  ##   ')
-  print('##     ## #####    ')
-  print('##     ## ##  ##   ')
-  print('##     ## ##   ##  ')
-  print(' #######  ##    ## ')
-  print('')
+  tLog.info('')
+  tLog.info(' #######  ##    ## ')
+  tLog.info('##     ## ##   ##  ')
+  tLog.info('##     ## ##  ##   ')
+  tLog.info('##     ## #####    ')
+  tLog.info('##     ## ##  ##   ')
+  tLog.info('##     ## ##   ##  ')
+  tLog.info(' #######  ##    ## ')
+  tLog.info('')
 end
